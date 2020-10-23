8f624f446e488f2b5f44bb093f31ac4479f98dd1
duane-faria <duane.faria@etec.sp.gov.br>
2020-10-23 14:45:05 -0300

    Criando tela de visualizar imagem

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import ViewImage from './src/views/ViewImage';
 import Welcome from './src/views/Welcome';
 
 export default function App() {
-  return <Welcome />;
+  return <ViewImage />;
 }
 
 const styles = StyleSheet.create({});
