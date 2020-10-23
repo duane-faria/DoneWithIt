7268f0672828d49f4812c6cf69a7b543a2c974b5
duane-faria <duane.faria@etec.sp.gov.br>
2020-10-23 12:10:53 -0300

    Iniciando o projeto

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Teste from './Teste';
 
 export default function App() {
+  let x = 1;
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>O expo está rodando no meu cel também</Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'tomato',
     alignItems: 'center',
     justifyContent: 'center',
+    color: '#fff',
+  },
+  text: {
+    color: '#fff',
   },
 });
