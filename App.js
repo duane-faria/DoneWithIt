b23f5b4d720e90c7c6348ab8307f3cc5595e83b5
duane-faria <duane.faria@etec.sp.gov.br>
2020-10-25 16:47:56 -0300

    Criando tela de detalhe da listagem

--- App.js
+++ App.js
@@ -2,21 +2,12 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import AppButton from './src/components/AppButton';
 import Card from './src/components/Card';
+import ListingDetails from './src/views/ListingDetails';
 import ViewImage from './src/views/ViewImage';
 import Welcome from './src/views/Welcome';
 
 export default function App() {
-  return (
-    <View
-      style={[{ backgroundColor: '#f8f4f4', padding: 20 }, styles.container]}
-    >
-      <Card
-        title='Red jacket for sale'
-        subTitle='R$100'
-        image={require('./src/assets/jacket.jpg')}
-      />
-    </View>
-  );
+  return <ListingDetails />;
 }
 
 const styles = StyleSheet.create({
