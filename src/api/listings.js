108a8f77a14a7b8a554c77abee2f94741adeb82c
duane-faria <duane.faria@etec.sp.gov.br>
2020-11-01 20:56:14 -0300

    Adicionando post com form data no serviço da api

--- src/api/listings.js
+++ src/api/listings.js
@@ -1,9 +1,32 @@
-import clinte from './client';
+import client from './client';
 
 const endpoint = '/listings';
 
-const getListings = () => clinte.get(endpoint);
+const getListings = () => client.get(endpoint);
+
+const addListing = (listing) => {
+  const data = new FormData();
+
+  data.append('title', listing.title);
+  data.append('price', listing.price);
+  data.append('categoryId', listing.category.value);
+  data.append('description', listing.category.value);
+
+  listing.images.forEach((image, index) => {
+    data.append('images', {
+      name: 'image' + index,
+      type: 'image/jpeg',
+      uri: image,
+    });
+  });
+
+  if (listing.location)
+    data.append('location', JSON.stringify(listing.location));
+
+  client.post(endpoint, data);
+};
 
 export default {
+  addListing,
   getListings,
 };
